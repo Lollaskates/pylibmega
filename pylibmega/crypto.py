8f784578b09179493b15b1449120eec20efbe052
Lollaskates <lollaskates@gmail.com>
2013-06-12 00:23:49 -0400

    changed to xrange and added benchmark test

--- pylibmega/crypto.py
+++ pylibmega/crypto.py
@@ -28,18 +28,18 @@ def aes_cbc_decrypt_a32(data, key):
 def stringhash(str, aeskey):
     s32 = str_to_a32(str)
     h32 = [0, 0, 0, 0]
-    for i in range(len(s32)):
+    for i in xrange(len(s32)):
         h32[i % 4] ^= s32[i]
-    for r in range(0x4000):
+    for r in xrange(0x4000):
         h32 = aes_cbc_encrypt_a32(h32, aeskey)
     return a32_to_base64((h32[0], h32[2]))
 
 def prepare_key(arr):
     pkey = [0x93C467E3, 0x7DB0C7A4, 0xD1BE3F81, 0x0152CB56]
-    for r in range(0x10000):
-        for j in range(0, len(arr), 4):
+    for r in xrange(0x10000):
+        for j in xrange(0, len(arr), 4):
             key = [0, 0, 0, 0]
-            for i in range(4):
+            for i in xrange(4):
                 if i + j < len(arr):
                     key[i] = arr[i + j]
             pkey = aes_cbc_encrypt_a32(pkey, key)
@@ -48,12 +48,12 @@ def prepare_key(arr):
 def encrypt_key(a, key):
     return sum(
         (aes_cbc_encrypt_a32(a[i:i + 4], key)
-            for i in range(0, len(a), 4)), ())
+            for i in xrange(0, len(a), 4)), ())
 
 def decrypt_key(a, key):
     return sum(
         (aes_cbc_decrypt_a32(a[i:i + 4], key)
-            for i in range(0, len(a), 4)), ())
+            for i in xrange(0, len(a), 4)), ())
 
 def encrypt_attr(attr, key):
     attr = 'MEGA' + json.dumps(attr)
@@ -119,6 +119,6 @@ def sizeof_fmt(num):
 def make_id(length):
     text = ''
     possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
-    for i in range(length):
+    for i in xrange(length):
         text += random.choice(possible)
     return text
